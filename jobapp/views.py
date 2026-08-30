d3fc77e4c0317781d85da8f704de4226b7929250
nishchitha1525 <nishchithadl@gmail.com>
2026-08-30 19:21:13 +0530

    Update views.py

--- jobapp/views.py
+++ jobapp/views.py
@@ -929,14 +929,13 @@ def dashboard(request):
 # LOGOUT
 # =========================================================
 
+# =========================================================
+# LOGOUT
+# =========================================================
+
 def logout_user(request):
 
     logout(request)
-
-    messages.success(
-        request,
-        "You have been logged out successfully."
-    )
 
     return redirect("home")
 
